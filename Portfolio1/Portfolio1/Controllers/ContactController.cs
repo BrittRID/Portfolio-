fe548b0ef5e39532b87c37fc01905338d9281d87
Brittany Ridley <britt.ridley@outlook.com>
2022-01-17 21:32:00 -0600

    portfolio

---
+++ Portfolio1/Portfolio1/Controllers/ContactController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio1.Controllers
+{
+    public class ContactController : Controller
+    {
+        // search on youtube AspNetnet.MVC 
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Portfolio1/Portfolio1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Portfolio1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
